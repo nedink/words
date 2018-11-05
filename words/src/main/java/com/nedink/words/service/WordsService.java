d787bf4dffe546649a85e8a61992c960513dd1e6
David Bryant <nedink@gmail.com>
2018-11-05 14:48:41 -0500

    feat(words): update API - count, regex

---
+++ words/src/main/java/com/nedink/words/service/WordsService.java
@@ -0,0 +1,73 @@
+package com.nedink.words.service;
+
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Service
+public class WordsService {
+
+    public WordsService() {
+    }
+
+    public long getCount() {
+        InputStream inputStream = null;
+        Stream<String> wordStream = null;
+
+        try {
+            File file = new File(getClass()
+                    .getClassLoader()
+                    .getResource("words.txt").getFile());
+            inputStream = new FileInputStream(file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            wordStream = reader.lines();
+
+            return wordStream.count();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public List getByRegex(String regex) {
+        InputStream inputStream = null;
+
+        try {
+            
+            File file = new File(getClass()
+                    .getClassLoader()
+                    .getResource("words.txt").getFile());
+            inputStream = new FileInputStream(file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            Stream<String> wordStream = reader.lines();
+            Stream<String> matches = wordStream.filter(word -> word.matches(regex));
+            return Arrays.asList(matches.toArray());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return null;
+    }
+}
---
+++ words/src/main/java/com/nedink/words/web/WordsController.java
@@ -0,0 +1,33 @@
+package com.nedink.words.web;
+
+import com.nedink.words.service.WordsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class WordsController {
+
+    private WordsService wordsService;
+
+    @Autowired
+    public WordsController(WordsService wordsService) {
+        this.wordsService = wordsService;
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity getCount() {
+        return new ResponseEntity(wordsService.getCount(), HttpStatus.OK);
+    }
+
+    @GetMapping(params = "regex")
+    public ResponseEntity getByRegex(@RequestParam String regex) {
+        List<String> result = wordsService.getByRegex(regex);
+        return new ResponseEntity(result, HttpStatus.OK);
+    }
+}
