a7788fdb5de829140e4cabecf90364f0d1fc243c
David Bryant <nedink@gmail.com>
2018-11-07 18:01:37 -0500

    feat(words-front): add

---
+++ words-front/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <words/>
+  </div>
+</template>
+
+<script>
+import Words from './components/Words.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Words
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  color: #cdcfe0;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+:root {
+  background-color: #2c3e50;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+*:focus {
+  transition-duration: 100ms;
+  box-shadow: 0 0 0 4px #00ff0080;
+  color: #f1f1f7;
+}
+*:hover {
+  color: #f1f1f7;
+}
+input {
+  font-size: 1rem;
+  background-color: #1f252b;
+    color: #f1f1f7;
+border: none;
+  border-radius: 0.25rem;
+  outline: none;
+  height: 38px;
+  padding: 0.5rem;
+}
+button {
+  font-size: 1rem;
+  background-color: #1f252b;
+  border: none;
+  border-radius: 0.25rem;
+  outline: none;
+  cursor: pointer;
+  height: 38px;
+  padding: 0.5rem 1.5rem;
+}
+button:hover {
+}
+#app {
+
+}
+</style>
---
+++ words-front/src/components/Words.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <div class="input-area">
+      <input class="regex-input" type="text" v-model="input">
+      <button class="submit">Search</button>
+    </div>
+    <div class="result">
+      <table>
+        <tr v-for="row in words.length / 4" :key="row">
+
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+const BACKEND = 'https://api.words-back.cfapps.io'
+
+export default {
+  data() {
+    return {
+      input: 'word.*',
+      words: []
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+    fetchWords() {
+      fetch(BACKEND + '/words?regex=' + new RegExp(this.input))
+      this.words = null;
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  max-width: 400px;
+  margin: auto;
+}
+.regex-input {
+  font-family: 'Courier New', Courier, monospace;
+  width: 100%;
+}
+.submit {
+  margin-left: 1rem;
+}
+.input-area {
+  display: flex;
+  
+}
+</style>
+
